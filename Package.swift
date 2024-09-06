2e6418eaf66de085da7022ca68c981c18ad3cecd
Adam Foot <admin@adamfoot.dev>
2024-09-06 09:51:51 +0100

    Updated package compatability

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
             dependencies: [],
             resources: [.process("Resources")],
             swiftSettings: [
-                .swiftLanguageVersion(.v6)
+                .swiftLanguageMode(.v6)
             ]
         ),
         .testTarget(
